cddd7f6809dd9b8988d94878ebbab67b1c8fa1ba
Elizabeth Abrego <an0174032023@unab.edu.sv>
2024-09-28 13:48:50 -0600

    Guía III 'Insertar un cliente ', finalizada.

--- EFDemo/Form1.Designer.cs
+++ EFDemo/Form1.Designer.cs
@@ -33,6 +33,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnTodos = new System.Windows.Forms.Button();
             this.tbxIDCustomers = new System.Windows.Forms.TextBox();
+            this.txbCustomerID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbCompanyName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbContactName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbContactTitle = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
             this.SuspendLayout();
             // 
@@ -80,11 +91,112 @@
             this.tbxIDCustomers.Size = new System.Drawing.Size(146, 20);
             this.tbxIDCustomers.TabIndex = 4;
             // 
+            // txbCustomerID
+            // 
+            this.txbCustomerID.Location = new System.Drawing.Point(849, 69);
+            this.txbCustomerID.Name = "txbCustomerID";
+            this.txbCustomerID.Size = new System.Drawing.Size(131, 20);
+            this.txbCustomerID.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(767, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Customer ID";
+            // 
+            // txbCompanyName
+            // 
+            this.txbCompanyName.Location = new System.Drawing.Point(849, 95);
+            this.txbCompanyName.Name = "txbCompanyName";
+            this.txbCompanyName.Size = new System.Drawing.Size(131, 20);
+            this.txbCompanyName.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(768, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Company Name";
+            // 
+            // txbContactName
+            // 
+            this.txbContactName.Location = new System.Drawing.Point(849, 121);
+            this.txbContactName.Name = "txbContactName";
+            this.txbContactName.Size = new System.Drawing.Size(131, 20);
+            this.txbContactName.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(768, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Contact Name";
+            // 
+            // txbContactTitle
+            // 
+            this.txbContactTitle.Location = new System.Drawing.Point(849, 147);
+            this.txbContactTitle.Name = "txbContactTitle";
+            this.txbContactTitle.Size = new System.Drawing.Size(131, 20);
+            this.txbContactTitle.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(768, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Contact Title";
+            // 
+            // txbAddress
+            // 
+            this.txbAddress.Location = new System.Drawing.Point(849, 173);
+            this.txbAddress.Name = "txbAddress";
+            this.txbAddress.Size = new System.Drawing.Size(131, 20);
+            this.txbAddress.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(768, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Address";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(873, 199);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(92, 25);
+            this.btnInsertar.TabIndex = 15;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(992, 450);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txbAddress);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txbContactTitle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbContactName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbCompanyName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbCustomerID);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.tbxIDCustomers);
             this.Controls.Add(this.btnTodos);
             this.Controls.Add(this.label1);
@@ -105,6 +217,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnTodos;
         private System.Windows.Forms.TextBox tbxIDCustomers;
+        private System.Windows.Forms.TextBox txbCustomerID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbCompanyName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbContactName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbContactTitle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnInsertar;
     }
 }
 
--- EFDemo/Form1.cs
+++ EFDemo/Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -31,5 +32,26 @@ namespace EFDemo
             List<Customers> listaId = new List<Customers> { cliente };
             dgvCustomers.DataSource = listaId;
         }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            var cliente = crearCliente();
+            var resultado = cr.InsertarCliente(cliente);
+            MessageBox.Show($"Se insertó {resultado} correctamente.");
+        }
+
+        private Customers crearCliente()
+        {
+            var cliente = new Customers()
+            {
+                CustomerID = txbCustomerID.Text,
+                CompanyName = txbCompanyName.Text,
+                ContactName = txbContactName.Text,
+                Address = txbAddress.Text,
+                ContactTitle = txbContactTitle.Text,
+            };
+            return cliente;
+        }
+
     }
 }
